dc18659c0b27f25a98bdec85c98d780979e08c64
chenyongda2018 <chenchenyongda@gmail.com>
2021-03-20 00:17:39 +0800

    complete exercises 1.1-1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,52 @@
 import React from 'react'
-const App = () => {
 
-  const now = Date()
-  const a = 10
-  const b = 10
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <>
-      <p>Hello world,now is {now.toString()}</p>
-      <p>{a} plus {b} is {a+b}</p>
-      <Hello name="cyd" />
-      <Hello/>
-      <Hello/>
-    </>
+    <div>
+      <Header course={course} />
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+
+
+
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
   )
 }
 
-const Hello = (props) => {
-  return(
+const Content = (props) => {
+  return (
     <div>
-      <p>Hello world,Welcome to {props.name}</p>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
     </div>
   )
 }
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>The exersices total is {props.total}</p>
+  )
+}
+
 export default App;
