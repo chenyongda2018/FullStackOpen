651482aafe5437bf239824a56ad66ad91ca28eff
chenyongda2018 <chenchenyongda@gmail.com>
2021-03-20 21:54:17 +0800

    handle Event & Componment State

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,68 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
 
-  const course = {
-    name :  'Half Stack application development',
-    parts : [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+
+  const increasement = () => {
+    //状态改变会是页面重新渲染
+    setCounter(counter + 1)
   }
 
+  const decreasement = () => {
+    setCounter(counter - 1)
+  }
+
+  const reset = () => { setCounter(0) }
+
   return (
     <div>
-      <Header course={course} />
-      <Content part={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-
-}
-
-const Header = (props) => {
-  console.log(props)
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part[0]}  />
-      <Part part={props.part[1]}  />
-      <Part part={props.part[2]}  />
+      <Display counter={counter} />
+      <Button handleClick={increasement} text="plus" />
+      <Button handleClick={decreasement} text="minus" />
+      <Button handleClick={reset} text="reset" />
     </div>
   )
 }
 
-const Part = (props) => {
-  return (
-    <p>{props.part.name} {props.part.exercises}</p>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Total = (props) => {
-  let total = 0
-  console.log("total ,",props.parts)
-
-  props.parts.forEach(p => {
-    total += p.exercises
-  });
-  return (
-    <p>The exersices total is {total}</p>
-  )
-}
+const Button = ({handleClick,text}) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 export default App;
