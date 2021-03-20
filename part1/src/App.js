9aa78fd00b9ee35fccd4fa0983c41672dff55c48
chenyongda2018 <chenchenyongda@gmail.com>
2021-03-20 18:21:05 +0800

    refactor componments code

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,51 +1,67 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const course = {
+    name :  'Half Stack application development',
+    parts : [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
-
-
 
 }
 
 const Header = (props) => {
+  console.log(props)
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part[0]} exercises={props.exercises[0]} />
-      <Part part={props.part[1]} exercises={props.exercises[1]} />
-      <Part part={props.part[2]} exercises={props.exercises[2]} />
+      <Part part={props.part[0]}  />
+      <Part part={props.part[1]}  />
+      <Part part={props.part[2]}  />
     </div>
   )
 }
 
 const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 
 const Total = (props) => {
+  let total = 0
+  console.log("total ,",props.parts)
+
+  props.parts.forEach(p => {
+    total += p.exercises
+  });
   return (
-    <p>The exersices total is {props.total}</p>
+    <p>The exersices total is {total}</p>
   )
 }
 
