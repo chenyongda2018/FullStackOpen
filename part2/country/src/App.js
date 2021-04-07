e2af023eca60d90a4cb67a5e01d4fa6d4f3680cd
chenyongda2018 <chenchenyongda@gmail.com>
2021-04-07 21:39:47 +0800

    add dynamic search func

--- part2/country/src/App.js
+++ part2/country/src/App.js
@@ -7,26 +7,19 @@ const App = () => {
 
   const [searchKey,setSearchKey] = useState("China");//搜索关键词
 
-  const searchHook = () => {
-
-    if(searchKey.length === 0) {
-      return;
-    }
-
-    axios
-      .get(`https://restcountries.eu/rest/v2/name/${searchKey}`)
-      .then(response => {
-        console.log("data: ",response.data);
-      })
-  };
-
-  //第二个参数
-  useEffect(searchHook,[searchKey]);
+  const [data,doSearch] = useApi(baseUrl.concat(searchKey),[]);
 
   const handleSearchKeyChange = (event) => {
     const keyWord = event.target.value;
     console.log("searchKey: ",keyWord);
     setSearchKey(keyWord);
+  }
+
+  const handleSearch = () => {
+    if(searchKey.length === 0) {
+      return;
+    }
+    doSearch(baseUrl.concat(searchKey));
   }
 
 
@@ -35,9 +28,59 @@ const App = () => {
       <div>
         find countries:
           <input value={searchKey} onChange={handleSearchKeyChange}/>
+          <button onClick={handleSearch}>Search</button>
       </div>
+      <CountryList data={data}/>
     </div>
   );
+}
+
+const CountryList = ({data}) => {
+  if(data.length > 10) {
+    return (<div>Too many matches,specify another filter</div>);
+  } else if(data.length > 1) {
+    return (
+      <div>
+        <ul>
+          {data.map((it) => 
+            <li key={it.numericCode}>{it.name}</li>)}
+        </ul>
+      </div>
+    );
+  } else if(data.length === 1){
+    return <CountryItem data={data[0]}/>
+  } else {
+    return(<div>No matches.</div>)
+  }
+  
+}
+
+const CountryItem = ({data}) => {
+  return(
+    <div>
+      <h3>{data.name}</h3>
+      <p>capital:{data.capital}</p>
+      <p>population:{data.population}</p>
+    </div>
+  )
+}
+
+const useApi = (initialUrl,initialData) => {
+  
+  const [url ,setUrl] = useState(initialUrl);
+  const [result ,setResult] = useState(initialData);
+
+  useEffect(()=> {
+    const fetchData = async() => {
+      const response = await axios.get(url);
+      console.log('result: ',response.data);
+      setResult(response.data);
+    }
+
+    fetchData();
+  },[url]);
+
+  return [result,setUrl];
 }
 
 
